7211d6ddacf0d95fbac200504addb1af15125f38
michbat <michael.batn@hotmail.com>
2025-11-13 06:59:48 +0100

    feat: create vehicle subclasses (Voiture, Moto) with documentation and TODOs

---
+++ Moto.cs
@@ -0,0 +1,14 @@
+﻿namespace _08_gestion_de_parking;
+
+/// <summary>
+/// Représente une moto dans le système de gestion de parking.
+/// Hérite de la classe abstraite Vehicule et implémente les comportements spécifiques aux motos.
+/// Les motos bénéficient généralement de tarifs préférentiels et d'emplacements dédiés.
+/// </summary>
+/// <param name="immatriculation">Numéro d'immatriculation de la moto</param>
+/// <param name="heureEntree">Heure d'entrée de la moto dans le parking</param>
+public class Moto(string immatriculation, DateTime heureEntree) : Vehicule(immatriculation, heureEntree)
+{
+    // TODO: Ajouter des propriétés spécifiques aux motos (cylindrée, type de moto, etc.)
+    // TODO: Implémenter le calcul du tarif réduit pour les motos
+}
---
+++ Voiture.cs
@@ -0,0 +1,13 @@
+﻿namespace _08_gestion_de_parking;
+
+/// <summary>
+/// Représente une voiture dans le système de gestion de parking.
+/// Hérite de la classe abstraite Vehicule et implémente les comportements spécifiques aux voitures.
+/// </summary>
+/// <param name="immatriculation">Numéro d'immatriculation de la voiture</param>
+/// <param name="heureEntree">Heure d'entrée de la voiture dans le parking</param>
+public class Voiture(string immatriculation, DateTime heureEntree) : Vehicule(immatriculation, heureEntree)
+{
+    // TODO: Ajouter des propriétés spécifiques aux voitures (nombre de portes, type de carburant, etc.)
+    // TODO: Implémenter le calcul du tarif de stationnement pour les voitures
+}
